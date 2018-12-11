83b0addb007b441be10092cb95e47b21d0734498
Jørgen Kvalsvik <jokva@equinor.com>
2018-12-11 15:45:31 +0100

    fixup! Add strong typedef base type

--- lib/extension/dlisio/ext/strong-typedef.hpp
+++ lib/extension/dlisio/ext/strong-typedef.hpp
@@ -5,6 +5,7 @@
 #include <utility>
 
 namespace dl {
+namespace detail {
 
 using std::swap;
 
@@ -21,17 +22,6 @@ public:
 
     constexpr static bool nothrow_swappable =
         noexcept (swap( std::declval< T& >(), std::declval< T& >() ));
-
-    strong_typedef() = default;
-
-    explicit strong_typedef( const T& x )
-        noexcept(strong_typedef::nothrow_copy_constructible);
-
-    explicit strong_typedef( T&& x )
-        noexcept(strong_typedef::nothrow_move_constructible);
-
-    explicit operator T&() noexcept (true);
-    explicit operator const T&() const noexcept (true);
 
     constexpr static bool nothrow_eq =
         noexcept (std::declval< const T& >() == std::declval< const T& >());
@@ -51,35 +41,23 @@ public:
     constexpr static bool nothrow_ge =
         noexcept (std::declval< const T& >() >= std::declval< const T& >());
 
-    bool operator == ( const strong_typedef& rhs ) const noexcept (nothrow_eq)
-    {
-        return this->value == rhs.value;
-    }
+    strong_typedef() = default;
 
-    bool operator != ( const strong_typedef& rhs ) const noexcept (nothrow_ne)
-    {
-        return this->value != rhs.value;
-    }
+    explicit strong_typedef( const T& x )
+        noexcept(strong_typedef::nothrow_copy_constructible);
 
-    bool operator < ( const strong_typedef& rhs ) const noexcept (nothrow_lt)
-    {
-        return this->value < rhs.value;
-    }
+    explicit strong_typedef( T&& x )
+        noexcept(strong_typedef::nothrow_move_constructible);
 
-    bool operator <= ( const strong_typedef& rhs ) const noexcept (nothrow_le)
-    {
-        return this->value <= rhs.value;
-    }
+    explicit operator T&() noexcept (true);
+    explicit operator const T&() const noexcept (true);
 
-    bool operator > ( const strong_typedef& rhs ) const noexcept (nothrow_gt)
-    {
-        return this->value > rhs.value;
-    }
-
-    bool operator >= ( const strong_typedef& rhs ) const noexcept (nothrow_ge)
-    {
-        return this->value >= rhs.value;
-    }
+    bool operator == ( const strong_typedef& ) const noexcept (nothrow_eq);
+    bool operator != ( const strong_typedef& ) const noexcept (nothrow_ne);
+    bool operator <  ( const strong_typedef& ) const noexcept (nothrow_lt);
+    bool operator <= ( const strong_typedef& ) const noexcept (nothrow_le);
+    bool operator >  ( const strong_typedef& ) const noexcept (nothrow_gt);
+    bool operator >= ( const strong_typedef& ) const noexcept (nothrow_ge);
 
 private:
     T value;
@@ -88,12 +66,88 @@ private:
      * swap is noexcept (although for strings it's only conditionally after
      * C++17, and not really at all before)
      */
-    friend void swap( strong_typedef& a, strong_typedef& b )
-        noexcept (nothrow_swappable)
-    {
+    friend void swap( strong_typedef& a, strong_typedef& b ) 
+        noexcept (nothrow_swappable) {
         swap( static_cast< T& >( a ), static_cast< T& >( b ) );
     }
 };
+
+template< typename Tag, typename T >
+strong_typedef< Tag, T >::strong_typedef( const T& x )
+        noexcept(strong_typedef::nothrow_copy_constructible)
+    : value( x ) {}
+
+template< typename Tag, typename T >
+strong_typedef< Tag, T >::strong_typedef( T&& x )
+        noexcept(strong_typedef::nothrow_move_constructible)
+    : value( std::move( x ) ) {}
+
+template< typename Tag, typename T >
+strong_typedef< Tag, T >::operator T&() noexcept(true) {
+    return this->value;
+}
+
+template< typename Tag, typename T >
+strong_typedef< Tag, T >::operator const T&() const noexcept(true) {
+    return this->value;
+}
+
+template< typename Tag, typename T >
+bool strong_typedef< Tag, T >::operator == 
+    ( const strong_typedef& rhs ) const
+    noexcept (nothrow_eq)
+{
+    return this->value == rhs.value;
+}
+
+template< typename Tag, typename T >
+bool strong_typedef< Tag, T >::operator !=
+    ( const strong_typedef& rhs ) const
+    noexcept (nothrow_ne)
+{
+    return this->value != rhs.value;
+}
+
+template< typename Tag, typename T >
+bool strong_typedef< Tag, T >::operator <
+    ( const strong_typedef& rhs ) const
+    noexcept (nothrow_lt)
+{
+    return this->value < rhs.value;
+}
+
+template< typename Tag, typename T >
+bool strong_typedef< Tag, T >::operator <=
+    ( const strong_typedef& rhs ) const
+    noexcept (nothrow_le)
+{
+    return this->value <= rhs.value;
+}
+
+template< typename Tag, typename T >
+bool strong_typedef< Tag, T >::operator >
+    ( const strong_typedef& rhs ) const
+noexcept (nothrow_gt)
+{
+    return this->value > rhs.value;
+}
+
+template< typename Tag, typename T >
+bool strong_typedef< Tag, T >::operator >=
+    ( const strong_typedef& rhs ) const
+    noexcept (nothrow_ge)
+{
+    return this->value >= rhs.value;
+}
+
+template< typename Tag, typename T >
+void swap( strong_typedef< Tag, T >& a,
+           strong_typedef< Tag, T >& b )
+    noexcept (strong_typedef< Tag, T >::nothrow_swappable)
+{
+}
+
+}
 
 }
 
